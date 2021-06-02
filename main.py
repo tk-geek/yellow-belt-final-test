2a407cb4e5fe0d134cc179966476d950207ae4e7
Tanvi Kachakayala <>
2021-06-02 23:04:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Enter information about the user")
+
+eyecolor = input (" please enter eye color ")
+
+print (" your eye color is ",eyecolor)
+
+height  = input (" please enter height ")
+
+print (" your  height is ", height)
+
+
+print (" you have height  " + height + " eye color is " + eyecolor)
